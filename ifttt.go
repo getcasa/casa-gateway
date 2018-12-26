787e6fa3170f409b119e495b26fafc5f37b1592a
Jimi M <jimi.maiquez@gmail.com>
2018-12-26 19:23:50 +0100

    add structs for models

---
+++ ifttt.go
@@ -0,0 +1,8 @@
+package main
+
+// IFTTT define request for ifttt webhook
+type IFTTT struct {
+	Value1 string `json:"value1"`
+	Value2 string `json:"value2"`
+	Value3 string `json:"value3"`
+}
---
+++ switch.go
@@ -0,0 +1,7 @@
+package main
+
+// Switch define xiaomi switch button
+type Switch struct {
+	Status  string `json:"status"`
+	Voltage int    `json:"voltage"`
+}
