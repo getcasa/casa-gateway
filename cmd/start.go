7e6e124505c5e09495940e75aea856f49962d6f7
Jimi M <jimi.maiquez@gmail.com>
2019-06-04 13:55:02 +0200

    test plugin system

--- cmd/start.go
+++ cmd/start.go
@@ -12,23 +12,44 @@ func init() {
 	rootCmd.AddCommand(startCmd)
 }
 
+func println(str string) {
+	fmt.Println(str)
+}
+
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "",
 	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
-		plug, err := plugin.Open("plugins/example/example.so")
+		plug, err := plugin.Open("plugins/xiaomi/xiaomi.so")
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		start, err := plug.Lookup("Start")
+		onStart, err := plug.Lookup("OnStart")
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		start.(func())()
+		onData, err := plug.Lookup("OnData")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// onStop, err := plug.Lookup("OnStop")
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	os.Exit(1)
+		// }
+
+		onStart.(func())()
+		for {
+			res := onData.(func() interface{})()
+			fmt.Println(res)
+		}
+		// onStop.(func())()
 	},
 }
--- go.mod
+++ go.mod
@@ -2,8 +2,10 @@ module github.com/ItsJimi/casa-gateway
 
 require (
 	github.com/fatih/color v1.7.0 // indirect
+	github.com/getcasa/sdk v0.0.0-20190603120433-a6275a3eed49
 	github.com/mattn/go-colorable v0.0.9 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
 )
