8880d9045150ed4d1fa66fbf4c339556152d2022
Jimi M <jimi.maiquez@gmail.com>
2019-11-06 16:15:21 +0100

    fix: re search ips from network at reconnection

--- gateway/websocket_client.go
+++ gateway/websocket_client.go
@@ -3,14 +3,12 @@ package gateway
 import (
 	"encoding/json"
 	"log"
-	"net"
 	"net/url"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
-	portscanner "github.com/anvie/port-scanner"
+	"github.com/ItsJimi/casa-gateway/utils"
 	"github.com/gorilla/websocket"
 )
 
@@ -30,23 +28,23 @@ type ActionMessage struct {
 // WS is the connector to write message across app
 var WS *websocket.Conn
 
-const serverPort = 4353
-
 // connectWebsocket connect casa gateway to casa server and retry on fails
 func connectWebsocket(port string) {
-	ips := discoverServer()
-
 	for {
 		var err error
-		for _, ip := range ips {
-			u := url.URL{Scheme: "ws", Host: ip + ":" + strconv.Itoa(serverPort), Path: "/v1/ws"}
-			WS, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
-			if err != nil {
-				log.Printf("dial err:" + err.Error())
-				log.Printf("wait 5 seconds to redail...")
-				time.Sleep(time.Second * 5)
-				continue
-			}
+		ips := utils.DiscoverServer()
+		if len(ips) == 0 {
+			log.Printf("wait 5 seconds to redail...")
+			time.Sleep(time.Second * 5)
+			continue
+		}
+		u := url.URL{Scheme: "ws", Host: ips[0] + ":" + strconv.Itoa(utils.ServerPort), Path: "/v1/ws"}
+		WS, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
+		if err != nil {
+			log.Printf("dial err:" + err.Error())
+			log.Printf("wait 5 seconds to redail...")
+			time.Sleep(time.Second * 5)
+			continue
 		}
 		break
 	}
@@ -62,60 +60,6 @@ func connectWebsocket(port string) {
 		log.Println("write:", err)
 		return
 	}
-}
-
-func containIPAddress(arr []string, search string) bool {
-	for _, addr := range arr {
-		if addr == search {
-			return true
-		}
-	}
-	return false
-}
-
-var waitg sync.WaitGroup
-
-func discoverServer() []string {
-	var ips []string
-	var ipAddresses []string
-	ifaces, err := net.Interfaces()
-	if err == nil {
-
-		for _, iface := range ifaces {
-			addrs, err := iface.Addrs()
-			if err == nil {
-				for _, addr := range addrs {
-					cleanAddr := addr.String()[:strings.Index(addr.String(), "/")]
-					if cleanAddr != "127.0.0.1" && !strings.Contains(cleanAddr, ":") && !net.ParseIP(cleanAddr).IsLoopback() {
-						cleanAddr = addr.String()[:strings.LastIndex(addr.String(), ".")+1]
-						if !containIPAddress(ipAddresses, cleanAddr) {
-							ipAddresses = append(ipAddresses, cleanAddr)
-						}
-					}
-				}
-			}
-		}
-
-		waitg.Add(len(ipAddresses) * 255)
-
-		for _, ipAddr := range ipAddresses {
-			for i := 0; i < 255; i++ {
-				go func(i int, ipAddr string) {
-					ip := ipAddr + strconv.Itoa(i)
-					ps := portscanner.NewPortScanner(ip, 3*time.Second, 4)
-					opened := ps.IsOpen(serverPort)
-					if opened {
-						ips = append(ips, ip)
-					}
-					waitg.Done()
-				}(i, ipAddr)
-
-			}
-		}
-		waitg.Wait()
-	}
-
-	return ips
 }
 
 // StartWebsocketClient start a websocket client to send and receive data between casa server and casa gateway
--- utils/utils.go
+++ utils/utils.go
@@ -3,9 +3,14 @@ package utils
 import (
 	cryptorand "crypto/rand"
 	"io/ioutil"
+	"net"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
 	"time"
 
+	portscanner "github.com/anvie/port-scanner"
 	"github.com/getcasa/sdk"
 	"github.com/oklog/ulid/v2"
 )
@@ -59,4 +64,62 @@ func FindFieldFromName(fields []sdk.Field, name string) sdk.Field {
 		}
 	}
 	return sdk.Field{}
+}
+
+func containIPAddress(arr []string, search string) bool {
+	for _, addr := range arr {
+		if addr == search {
+			return true
+		}
+	}
+	return false
+}
+
+var waitg sync.WaitGroup
+
+// ServerPort is the default port use by casa server
+const ServerPort = 4353
+
+// DiscoverServer get ips of casa servers
+func DiscoverServer() []string {
+	var ips []string
+	var ipAddresses []string
+	ifaces, err := net.Interfaces()
+	if err == nil {
+
+		for _, iface := range ifaces {
+			addrs, err := iface.Addrs()
+			if err == nil {
+				for _, addr := range addrs {
+					cleanAddr := addr.String()[:strings.Index(addr.String(), "/")]
+					if cleanAddr != "127.0.0.1" && !strings.Contains(cleanAddr, ":") && !net.ParseIP(cleanAddr).IsLoopback() {
+						cleanAddr = addr.String()[:strings.LastIndex(addr.String(), ".")+1]
+						if !containIPAddress(ipAddresses, cleanAddr) {
+							ipAddresses = append(ipAddresses, cleanAddr)
+						}
+					}
+				}
+			}
+		}
+
+		waitg.Add(len(ipAddresses) * 255)
+
+		for _, ipAddr := range ipAddresses {
+			for i := 0; i < 255; i++ {
+				go func(i int, ipAddr string) {
+					ip := ipAddr + strconv.Itoa(i)
+					ps := portscanner.NewPortScanner(ip, 3*time.Second, 4)
+					opened := ps.IsOpen(ServerPort)
+					if opened {
+						ips = append(ips, ip)
+					}
+					waitg.Done()
+				}(i, ipAddr)
+
+			}
+		}
+		waitg.Wait()
+	}
+
+	return ips
 }
