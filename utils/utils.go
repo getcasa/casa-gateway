418b2d225cdd518b1c2fbaa1f7620f33b2accd88
Alex <amoinier@outlook.fr>
2019-11-18 10:28:48 +0100

    return GatewayID if already find

--- utils/utils.go
+++ utils/utils.go
@@ -16,6 +16,9 @@ import (
 	"github.com/oklog/ulid/v2"
 )
 
+//GatewayID save gateway's id
+var GatewayID string
+
 // NewULID create an ulid
 func NewULID() ulid.ULID {
 	id, _ := ulid.New(ulid.Timestamp(time.Now()), cryptorand.Reader)
@@ -24,20 +27,28 @@ func NewULID() ulid.ULID {
 
 // GetIDFile get ID from config file
 func GetIDFile() string {
-	var id string
+	if GatewayID != "" {
+		return GatewayID
+	}
 
 	file, err := os.OpenFile(".casa", os.O_APPEND, 0644)
 	if err != nil {
-		id = string(NewULID().String())
-		err = ioutil.WriteFile(".casa", []byte(id), 0644)
-		Check(err, "error")
-	} else {
-		data := make([]byte, 100)
-		count, err := file.Read(data)
-		Check(err, "error")
-		id = string(data[:count])
+		GatewayID = string(NewULID().String())
+		err = ioutil.WriteFile(".casa", []byte(GatewayID), 0644)
+		if err != nil {
+			return ""
+		}
+
+		return GatewayID
 	}
-	return id
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		return ""
+	}
+	GatewayID = string(data[:count])
+
+	return GatewayID
 }
 
 // Check check error
